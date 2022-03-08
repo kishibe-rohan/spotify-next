ec302cd7a9a5533acdc86dd390534094d096c318
kishibe-rohan <devrohan2000@gmail.com>
2022-03-08 21:36:32 +0530

    added custom loader

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,10 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-import { getProviders, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { getProviders, signIn, useSession } from 'next-auth/react'
+import { useEffect } from 'react'
+
+import Loader from '../../components/Loader'
 
 function Signin({ providers }) {
   const { data: session } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (session) {
+      router.push('/')
+    }
+  }, [session])
+
+  if (session) return <Loader />
 
   return (
     <div className="flex h-screen flex-col items-center space-y-8 bg-black pt-40">
@@ -23,7 +36,10 @@ function Signin({ providers }) {
 
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
-          <button className="rounded-full border border-transparent bg-[#1db954] py-4 px-6 text-xs font-bold uppercase tracking-wider text-white transition duration-300 ease-out hover:scale-105 hover:bg-[#0db146] md:text-base">
+          <button
+            className="rounded-full border border-transparent bg-[#1db954] py-4 px-6 text-xs font-bold uppercase tracking-wider text-white transition duration-300 ease-out hover:scale-105 hover:bg-[#0db146] md:text-base"
+            onClick={() => signIn(provider.id)}
+          >
             Sign In With {provider.name}
           </button>
         </div>
