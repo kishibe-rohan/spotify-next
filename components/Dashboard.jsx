ebc743a3d8c3fb45f61e91dd3d1ea63647cd6943
kishibe-rohan <devrohan2000@gmail.com>
2022-03-08 16:38:17 +0530

    basic layout

---
+++ components/Dashboard.jsx
@@ -0,0 +1,15 @@
+import Sidebar from './Sidebar'
+import Feed from './Feed'
+import Rightbar from './Rightbar'
+
+function Dashboard() {
+  return (
+    <main>
+     <Sidebar/>
+     <Feed/>
+     <Rightbar/>
+    </main>
+  )
+}
+
+export default Dashboard
