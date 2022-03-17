c6d022669bd0e51b90ef3dd11e6132fb8c26b7e5
kishibe-rohan <devrohan2000@gmail.com>
2022-03-17 17:05:53 +0530

    added auth to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
-import Head from 'next/head'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+
 import Image from 'next/image'
 
+import Head from 'next/head'
 import Dashboard from '../components/Dashboard'
+import Loader from '../components/Loader'
 
 const Home = () => {
+  const router = useRouter()
+  const { status, data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push('/auth/signin')
+    },
+  })
+
+  if (status === 'loading') {
+    return <Loader />
+  }
+
   return (
     <div>
       <Head>
